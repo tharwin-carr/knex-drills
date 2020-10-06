2f66f03655cbdfb00576c0de846446137617333d
Tharwin <tharwin.carr08@gmail.com>
2020-10-06 13:05:01 -0700

    building services checkpoint

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,23 @@
+INSERT INTO blogful_articles (title, date_published, content)
+VALUES
+    ('Apple', '2017-01-18', 'new apple article'),
+    ('Facebook', '2020-03-12', 'new facebook article'),
+    ('Amazon', '2020-03-11', 'new amazon article'),
+    ('Google', '2014-06-22', 'new google article'),
+    ('Forbes', '2018-07-21', 'new forbes article'),
+    ('Among Us', '2020-01-01', 'new among us article'),
+    ('Pixel 5', '2020-10-02', 'new pixel 5 article'),
+    ('Bloc', '2010-12-12', 'new Bloc article'),
+    ('GitHub', '2008-08-24', 'new GitHub article'),
+    ('LeBron James', '2009-03-30', 'new lebron james article'),
+    ('Kobe Bryant', '2020-08-24', 'new kobe bryant article'),
+    ('Allen Iverson', '2002-11-29', 'new allen iverson article'),
+    ('Xbox', '2004-12-24', 'new xbox article'),
+    ('Playstation', '2007-05-18', 'new playstation article'),
+    ('Nintendo', '2020-01-02', 'new nintendo article'),
+    ('Twitter', '2019-10-30', 'new twitter article'),
+    ('Instagram', '2014-07-17', 'new instagram article'),
+    ('Snapchat', '2019-09-13', 'new snapchat article'),
+    ('Basketball', '2009-02-26', 'new basketball article'),
+    ('Football', '2018-07-29', 'new football article')
+    ;
